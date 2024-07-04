d43b0b00f519969b0ed165a46683aa8fa61971f6
Elai <66779575+elj40@users.noreply.github.com>
2024-07-04 11:46:53 +0200

    Add rainfall data collection

---
+++ data/NYA_FRONT_CSV.js
@@ -0,0 +1,20 @@
+const num_input = "<input class='cell-input'type='number'>"
+const NYA_FRONT_CSV = `Call sign:;;Name:;;Sheet no.;
+            Date;${input_date};${input_date};${input_date};${input_date};${input_date};${input_date};${input_date}
+            Scout 1;;;;;;;
+            Scout 2;;;;;;;
+            Scout 3;;;;;;;
+            Scout 4;;;;;;;
+            Scout 5;;;;;;;
+            Scout 6;;;;;;;
+            M'vula (mm);${num_input};${num_input};${num_input};${num_input};${num_input};${num_input};${num_input}
+            Incident Report No.;;;;;;;
+            Nyama zina?;;;;;;;
+            ;;;;;;;
+            ;;;;;;;
+            Nyama yafa?;;;;;;;
+            ;;;;;;;
+            ;;;;;;;
+            `
+
+const NYA_FRONT_STYLE = nyamvuStyleFront()
--- rearrange.js
+++ rearrange.js
@@ -1,65 +1,3 @@
-const front_csv = `Call sign:;bravo charlie;Name:;buffalo camp;Sheet no.;four-4;;
-Date;08/22/2023;08/23/2023;08/24/2023;08/25/2023;08/26/2023;08/27/2023;08/28/2023
-Scout 1;li;lg;lh;lt;lw;le;ll
-Scout 2;;;;;;;
-Scout 3;;;;;;;
-Scout 4;;;;;;;
-Scout 5;;;;;;;
-Scout 6;;;;;;;
-M'vula?;mm-;mm-;mm-;mm-;mm-;mm-;mm-
-Incident Report No.;x;x;no patrol;x;x;x;NO PATROL
-Nyama zina?;x;x;NO PATROL;x;x;x;NO PATROL
-;;;;;;;
-;;;;;;;
-Nyama yafa?;x;x;NO PATROL;x;x;one bushbuck;NO PATROL
-;;;;;;h27;
-;;;;;;cause leopard;`
-
-const back_csv = `Species;B;M;B;M;B;M;B;M;B;M;B;M;B;M;B;M;B;M;B;M;B;M;B;M
-Impala;h27;2;h25;7;x;x;i27;9;x;x;x;x;h27;15;f26;18;x;x;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Kudu;x;x;h28;3;j29;5;j31;2;x;x;h28;1;x;x;d27;2;x;x;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Elephant;x;x;x;x;h28;11;x;x;f26;7;e28;17;x;x;;;;;;;;;;
-Hippo;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;;;
-Buffalo;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;;;
-Waterbuck;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;;;
-Roan;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Giraffe;x;x;x;x;x;x;d27;4;x;x;g27;5;;;;;;;;;;;;
-Warthog;h26;1;x;x;x;x;x;x;h27;1;x;x;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Zebra;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Bushbuck;x;x;i28;2;j31;1;k30;2;x;x;h28;3;x;x;g26;2;g26;1;f27;3;c27;1;x;x
-;h28;1;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;
-Puku;x;x;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;
-Wildebeest;x;x;x;x;x;x;x;x;x;x;;;;;;;;;;;;;;
-Duiker;x;x;k30;1;x;x;x;x;x;x;;;;;;;;;;;;;;
-Sable;x;x;x;x;x;x;x;x;;;Crested;h27;18;x;x;x;x;x;x;x;x;;;
-Klipspringer;x;x;j28;2;x;x;x;x;;;Helmeted;;;;;;;;;;;;;
-Grysbuck;i26;1;x;x;x;x;x;x;;;M'ng'omba;x;x;x;x;h28;5;x;x;x;x;;;
-S. monkey;x;x;x;x;x;x;h27;11;x;x;;;;;;;;;;;;;;
-Baboon;h25;1;x;x;i28;1;x;x;h28;1;x;x;b27;1;x;x;h28;1;;;;;;
-V. monkey;x;x;x;x;x;x;x;x;;;;;;;Kandwe;;;;;;;;;
-Crocodile;x;x;x;x;x;x;x;x;;;;;;;Kaingo;;;;;;;;;
-Porcupine;x;x;x;x;x;x;x;x;;;;;;;Kalamo;g26;1;;;;;;;;;
-Bushpig;x;x;x;x;x;x;x;x;;;;;;;Nimbulu;;;;;;;;;
-Pangolin;f12;1;x;x;x;x;x;x;;;;;;;Chimwi;;;;;;;;;
-Hartebeest;g34;2;x;x;x;x;x;x;;;;;;;Fungofungo;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;
-Wayenda kuti lero?;* Species not on the list must be added in the open space or placed under the "off-interest" section;;;;;;;;;;;;;;;;;;;;;;;
-Date;;;;;;;;;;;;;;;;;;;;;;;;
-08/22/2023;h27;h26;h25;h25;i26;h27;;;;;;;;;;;;;;;;;;
-08/23/2023;h28;i28;i28;j29;j30;j31;k30;j28;i27;h28;;;;;;;;;;;;;;
-08/24/2023;;;;;;;;;;;;;;;;;;;;;;;;
-08/25/2023;h28;h28;h28;h28;h28;;;;;;;;;;;;;;;;;;;
-08/26/2023;h27;h27;h27;g26;g26;f27;f26;d27;d27;c27;b27;c27;e28;g26;h27;;;;;;;;;
-08/27/2023;h28;h28;h27;g27;;;;;;;;;;;;;;;;;;;;
-08/28/2023;;;;;;;;;;;;;;;;;;;;;;;;`;
-
-
 let report_data = {};
 
 function createReports(front,back) {
@@ -74,7 +12,9 @@ function createReports(front,back) {
     const animal = createAnimalReport(front_p,back_p);
     const patrol = createPatrolReport(front_p,back_p);
     const carcass = createYafaReport(front_p);
+    const rainfall = createRainfallReport(front_p);
 
+    download(rainfall, report_data.sheet_id+"_rainfall.csv", "text/plain");
     download(animal, report_data.sheet_id+"_animal.csv", "text/plain");
     download(patrol, report_data.sheet_id+"_patrol.csv", "text/plain");
     download(carcass, report_data.sheet_id+"_carcass.csv", "text/plain");
@@ -123,16 +63,16 @@ function getAnimalData(fp,bp) {
 
     species.push(getSpecieData(parseArea(bp,1,32,50,1)));  //hartebeest
 
-    species.push(getSpecieData(parseArea(bp,17,22,50,1)));  //crested
-    species.push(getSpecieData(parseArea(bp,17,23,50,1)));  //helmeted
-    species.push(getSpecieData(parseArea(bp,17,24,50,1)));  //mngomba
+    species.push(getSpecieData(parseArea(bp,19,22,50,1)));  //crested
+    species.push(getSpecieData(parseArea(bp,19,23,50,1)));  //helmeted
+    species.push(getSpecieData(parseArea(bp,19,24,50,1)));  //mngomba
     
-    species.push(getSpecieData(parseArea(bp,21,27,50,1)));  //kandwe
-    species.push(getSpecieData(parseArea(bp,21,28,50,1)));  //kaingo
-    species.push(getSpecieData(parseArea(bp,21,29,50,1)));  //kalamo
-    species.push(getSpecieData(parseArea(bp,21,30,50,1)));  //nimbulu
-    species.push(getSpecieData(parseArea(bp,21,31,50,1)));  //chimwi
-    species.push(getSpecieData(parseArea(bp,21,32,50,1)));  //fungofungo
+    species.push(getSpecieData(parseArea(bp,23,27,50,1)));  //kandwe
+    species.push(getSpecieData(parseArea(bp,23,28,50,1)));  //kaingo
+    species.push(getSpecieData(parseArea(bp,23,29,50,1)));  //kalamo
+    species.push(getSpecieData(parseArea(bp,23,30,50,1)));  //nimbulu
+    species.push(getSpecieData(parseArea(bp,23,31,50,1)));  //chimwi
+    species.push(getSpecieData(parseArea(bp,23,32,50,1)));  //fungofungo
     
     return {dates: dates, sightings: species};
 }
@@ -214,6 +154,16 @@ function createYafaReport(fp) {
     return csv;
 }
 
+function createRainfallReport(fp) {
+	csv = "";
+	for (let i=1; i<=7; i++) {
+		csv += fp[1][i]+';';
+		if (fp[8][i].trim() == '') csv += '0\n';
+		else csv += fp[8][i]+'\n';
+	}
+	return csv
+}
+
 
 function parseArea(table, x,y, sx, sy) {
     let data = {name: "", data:[]}
@@ -231,6 +181,7 @@ function parseArea(table, x,y, sx, sy) {
         try {data.data.push(table[cy][cx]);}
         catch (err) {
             console.log(`Error on parsing: ${cx}, ${cy}`, table[cy][cx]);
+            alert(`Error on parsing: ${cx}, ${cy}`, table[cy][cx]);
             throw err;
         }
         
